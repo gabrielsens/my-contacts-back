1f353254c2d53648b12ced7d7529d1a78720aa05
Gabriel Sens <gabriel_sens@hotmail.com>
2022-02-23 14:00:20 -0300

    .

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Method', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
   response.setHeader('Access-Control-Allow-Max-Age', '10');
